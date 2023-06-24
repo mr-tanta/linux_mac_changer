554018110fd95e5d943a8ec3e2f260b5243a7fd6
Abraham Esandayinze Tanta <abraham@tantainnovatives.com>
2023-06-24 18:35:48 +0100

    added a function to generate random mac addresses

--- mac_changer.py
+++ mac_changer.py
@@ -1,30 +1,7 @@
 import subprocess
 import re
 import argparse
-
-
-def get_argument():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
-    parser.add_argument("-m", "--mac", dest="new_mac", help="New MAC Address")
-    options = parser.parse_args()
-
-    if not options.interface:
-        parser.error("[-] Please specify an interface, use --help or -h for more information")
-    elif not options.new_mac:
-        parser.error("[-] Please specify a new MAC address, use --help or -h for more information")
-
-    return options
-
-
-def change_mac(interface, new_mac):
-    print(f"[+] Changing the MAC address for {interface} to {new_mac}")
-    try:
-        subprocess.call(["ifconfig", interface, "down"])
-        subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-        subprocess.call(["ifconfig", interface, "up"])
-    except subprocess.CalledProcessError as e:
-        print("[-] An error occurred while changing the MAC address.")
+import random
 
 
 def current_mac(interface):
@@ -39,7 +16,52 @@ def current_mac(interface):
         print(f"[-] An error occurred while getting the current MAC address of {interface}.")
 
 
+def get_argument():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
+    parser.add_argument("-m", "--mac", dest="new_mac", help="New MAC Address")
+    parser.add_argument("-r", "--random", action="store_true", dest="random_mac", help="Set a random MAC Address")
+    options = parser.parse_args()
+
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help or -h for more information")
+    elif not options.new_mac and not options.random_mac:
+        parser.error("[-] Please specify either a new MAC address or use the random MAC option")
+
+    return options
+
+
+def change_mac(interface, new_mac):
+    print(f"[+] Changing the MAC address for {interface} to {new_mac}")
+    try:
+        subprocess.call(["ifconfig", interface, "down"])
+        subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+        subprocess.call(["ifconfig", interface, "up"])
+    except subprocess.CalledProcessError as e:
+        print("[-] An error occurred while changing the MAC address.")
+
+
+def generate_random_mac():
+    random_mac = [
+        random.choice("0123456789abcdef") + random.choice("02468ace") +
+        ":" +
+        random.choice("0123456789abcdef") + random.choice("02468ace") +
+        ":" +
+        random.choice("0123456789abcdef") + random.choice("02468ace") +
+        ":" +
+        random.choice("0123456789abcdef") + random.choice("02468ace") +
+        ":" +
+        random.choice("0123456789abcdef") + random.choice("02468ace") +
+        ":" +
+        random.choice("0123456789abcdef") + random.choice("02468ace")
+    ]
+    return "".join(random_mac)
+
+
 options = get_argument()
+if options.random_mac:
+    options.new_mac = generate_random_mac()
+
 current_mac_address = current_mac(options.interface)
 if current_mac_address:
     print(f"[+] Current MAC Address = {current_mac_address}")
